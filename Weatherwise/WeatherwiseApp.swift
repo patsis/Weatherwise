166c37d8cccde7dc6ae1f5159a88e618a5c3a066
harry patsis <patsis@gmail.com>
2024-06-21 19:40:13 +0300

    Initial Commit

---
+++ Weatherwise/WeatherwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherwiseApp.swift
+//  Weatherwise
+//
+//  Created by Harry Patsis on 21/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
